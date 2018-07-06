b030084618410b2b021c40203a7f6f2ecc094877
julngo <julian.t.ngo@live.com>
2018-07-05 23:56:03 -0700

    Typing is working

--- index.js
+++ index.js
@@ -20,11 +20,34 @@ var data = {
         }
     ]
 }
+var begStr = "vi julian.json";
+var string = JSON.stringify(data, undefined, 2);
 
-var stringText = JSON.stringify(data, undefined, 2);
-
-
+var typing = ' ';
+var i = 0; 
+//I STOLE THIS FROM SU
+function type(str) {
+  if(i < str.length){
+    typing = typing+str.charAt(i);
+    $('#textHere').text(typing+'_');
+  }else{
+    $('#textHere').text(typing);
+    return false;
+  }
+    var ran = 0;
+  //var ran = Math.floor(Math.random() * 150) + 75;
+    //this gives a random delay between each character so it's more realistic
+  //set ran to a fixed number if you want consistent speed.
+  setTimeout(function(){
+    type(str);
+  }, ran)
+  i++;
+};
     
 $(document).ready(function(){
-    $('.terminal-body pre').text(stringText);
+    i = 0;
+    type(string);
+    //$('#textHere').text(stringText);
 })
+
+
