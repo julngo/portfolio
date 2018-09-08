0812d01cd9f2f5d039d9c79a418979239c29084c
julngo <julian.t.ngo@live.com>
2018-09-07 23:55:39 -0700

    .

--- js/veda.js
+++ js/veda.js
@@ -10,7 +10,8 @@ function closeWindow(){
     $('.project-view').removeClass("transform-active");
     $('.proj-buttons').removeClass("active");
 }
-var pageHeader = '<div id="nav-bar"> <ul> <li><img style="float: left" id="headerPic" src="images/VedaAI_Logo.png"></li> <li onclick="fillPageOne()"><a><div>Home</div></a></li> <li onclick="fillPageTwo()"><a><div>Research</div></a></li> <li><a><div>App</div></a></li>  <li><a><div>Sketches</div></a></li> </ul> </div> <hr><div class="veda-content"></div>';
+//<img id="headerPic" src="images/VedaAI_Logo.png">
+var pageHeader = '<div id="nav-bar"> <ul> <li style="float: left; width: 200px;" ><img id="headerPic" src="images/VedaAI_Logo.png"></li> <li><a><div>Sketches</div></a></li> <li><a><div>App</div></a></li> <li onclick="fillPageTwo()"><a><div>Research</div></a></li> <li onclick="fillPageOne()"><a><div>Home</div></a></li> </ul> </div> <hr><div class="veda-content"></div>';
 
 var pageOne =  '<h1>Roles:</h1> <p>Group Manager, Lead App Developer, and Designer</p> <div id="project-body"> <iframe src="https://player.vimeo.com/video/259108778" width="640" height="360" frameborder="0" allowfullscreen></iframe> </div> <div><p> Bacon ipsum dolor amet leberkas alcatra hamburger corned beef jowl, cow beef beef ribs shank capicola short ribs turkey tongue. Tail filet mignon bacon, tongue pork loin boudin buffalo. Pork loin drumstick ribeye, doner short ribs pork meatloaf short loin biltong. Swine landjaeger jowl sausage buffalo. Chuck pork belly buffalo, shoulder pork chop jowl leberkas porchetta ground round tri-tip kielbasa meatball. Burgdoggen biltong rump shankle cow tenderloin. Ribeye pancetta meatball beef biltong, bacon burgdoggen ham sausage leberkas cow hamburger short loin tongue doner. </p> </div>';
 
