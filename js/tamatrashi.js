29d289aa1d787ab1a93a921bbea6d690e20d3a12
julngo <julian.t.ngo@live.com>
2018-09-15 16:14:24 -0700

    .

--- js/tamatrashi.js
+++ js/tamatrashi.js
@@ -2,6 +2,8 @@ function openWindow(){
     $('.project-view').addClass("transform-active");
     $('.proj-buttons').addClass("active");
 }
+var tamatrashiPageHeader = '<div id="nav-bar"><ul><li><div></div></li><li><div></div></li><li><div></div></li></ul></div>';
+var tamatrashiPageOne = '<div class="sect-1"></div><div class="sect-2"></div>';
 
 function projectThree(){
     openWindow();
@@ -15,3 +17,4 @@ function projectThree(){
         $('.tamatrashi-content').html();
     },1000.5); 
 }
+
