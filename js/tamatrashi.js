924a8f093b67b685fa3ad16aeb7083c5071f2813
julngo <julian.t.ngo@live.com>
2018-09-16 15:11:28 -0700

    tamatrashi work

--- js/tamatrashi.js
+++ js/tamatrashi.js
@@ -2,7 +2,7 @@ function openWindow(){
     $('.project-view').addClass("transform-active");
     $('.proj-buttons').addClass("active");
 }
-var tamatrashiPageHeader = '<div id="nav-bar"><ul><li><div></div></li><li><div></div></li><li><div></div></li></ul></div>';
+var tamatrashiPageHeader = '<div id="nav-bar"><ul><li><div>home</div></li><li><div>Ideas</div></li><li><img width="100%" src="images/title.png"></li><li><div>Production</div></li><li><div>Reflection</div></li></ul></div>';
 var tamatrashiPageOne = '<div class="sect-1"></div><div class="sect-2"></div>';
 
 function projectThree(){
@@ -13,7 +13,7 @@ function projectThree(){
     $('.project-name-fill').text("Tamatrashi");
     setTimeout(function(){
         i=0;
-        $('.project-content').html();
+        $('.project-content').html(tamatrashiPageHeader);
         $('.tamatrashi-content').html();
     },1000.5); 
 }
