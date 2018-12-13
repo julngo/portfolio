05508172e2bbda90cfe391d48238dc76a4401d98
Julian Ngo <julngo@uw.edu>
2018-12-13 01:51:33 -0800

    submission for the night

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -9,6 +9,7 @@ function closeWindowAM(){
     $('.preference-window').removeClass("active-state-1");
     $('.preference-buttons').removeClass("active");
     $('.tabs').remove();
+    reset();
 }
 
 $('.preference-buttons div').hover(
@@ -28,7 +29,7 @@ function openAboutMe(){
     $('head').append('<link class="aboutMe" href="css/aboutMe.css" rel="stylesheet">');
     
     $('.preference-name').append('<div class="tabs"> <div onclick="openFirstTab()" class="selected" id="firstTab">Overview</div> <div onclick="openTab2()" id="tab2">Resumè</div> <div onclick="openTab3()" id="tab3">Storage</div> <div onclick="openTab4()" id="tab4">Contact</div> <div onclick="openLastTab()"id="lastTab">Game</div> </div>');
-    openTab1
+    openFirstTab();
 }
 
 function reset(){
@@ -37,31 +38,45 @@ function reset(){
     $('#tab3').removeClass('selected');
     $('#tab4').removeClass('selected');
     $('#lastTab').removeClass('selected');
-    $('.preference-body').empty;
+    $('.preference-body').empty();
 }
 
 function openFirstTab(){
     reset();
     $('#firstTab').addClass('selected');
-    
+    $('.preference-body').html(firstTabContent);
 }
 
 function openTab2(){
     reset();
     $('#tab2').addClass('selected');
+    $('.preference-body').html(tab2Content);
 }
 
 function openTab3(){
     reset();
     $('#tab3').addClass('selected');
+    $('.preference-body').html(tab3Content);
 }
 
 function openTab4(){
     reset();
     $('#tab4').addClass('selected');
+    $('.preference-body').html(tab4Content);
 }
 
 function openLastTab(){
     reset();
     $('#lastTab').addClass('selected');
+    $('.preference-body').html(lastTabContent);
 }
+
+var firstTabContent = "<div id='overview'><img src='images/julian-border.png'><header><b>Julian</b> Ngo</header><p id='ver'><b>Version</b> 21.5</p> <p>TEST hek</p></div>";
+
+var tab2Content = "";
+
+var tab3Content = "";
+
+var tab4Content = "";
+
+var lastTabContent = "";
